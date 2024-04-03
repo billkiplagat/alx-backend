00bbec50266a5124e03f808d2552217dc368fad6
billkiplagat <bill.kiplagat.7@gmail.com>
2024-04-03 15:22:16 +0300

    Completed Pagination Tasks

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""Simple pagination module.
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves page data.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_idx, end_idx = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_idx >= len(data):
+            return []
+
+
+
+        return data[start_idx:end_idx]
+
---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Simple pagination module.
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves page data.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_idx, end_idx = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_idx >= len(data):
+            return []
+
+        return data[start_idx:end_idx]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Retrieves more information about a page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data_page = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data_page),
+            "page": page,
+            "data": data_page,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
+
